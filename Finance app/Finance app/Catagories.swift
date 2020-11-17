d4089b07f4748b41a75415b1d8cfef45e0ecaf5e
Jarren Jackson <j.jarren@yahoo.com>
2020-11-16 21:01:31 -0600

    Added slight redesign to Category Tab -- and also added in a Search Bar

--- Finance app/Finance app/Catagories.swift
+++ Finance app/Finance app/Catagories.swift
@@ -8,13 +8,31 @@
 
 import UIKit
 
-class Catagories: UIViewController,UITabBarControllerDelegate, UITableViewDelegate {
+class Catagories: UIViewController, UITabBarControllerDelegate, UITableViewDelegate {
+
+
     @IBOutlet weak var AddCatagory: UIButton!
     
+
     @IBOutlet weak var CatagoryDisplay: UITableView!
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    private func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "category", for: indexPath)
+        
+        cell.textLabel!.text = "Category Item"
+        return cell
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        CatagoryDisplay.delegate = self
+        CatagoryDisplay.layer.cornerRadius = 10
 
         // Do any additional setup after loading the view.
     }
@@ -31,3 +49,39 @@ class Catagories: UIViewController,UITabBarControllerDelegate, UITableViewDelega
     */
 
 }
+
+
+// Adds options in Inspector to better manipulate the circle button
+
+@IBDesignable extension UIButton {
+
+      @IBInspectable var borderWidth: CGFloat {
+         set {
+             layer.borderWidth = newValue
+         }
+         get {
+             return layer.borderWidth
+         }
+     }
+
+      @IBInspectable var cornerRadius: CGFloat {
+         set {
+             layer.cornerRadius = newValue
+         }
+         get {
+             return layer.cornerRadius
+         }
+     }
+
+      @IBInspectable var borderColor: UIColor? {
+         set {
+             guard let uiColor = newValue else { return }
+             layer.borderColor = uiColor.cgColor
+         }
+         get {
+             guard let color = layer.borderColor else { return nil }
+             return UIColor(cgColor: color)
+         }
+     }
+ }
+
