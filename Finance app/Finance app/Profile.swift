f0643457fc46ddd91601264720394f549380b118
Jarren Jackson <j.jarren@yahoo.com>
2020-11-20 22:44:03 -0600

    Profile object (#7)

--- Finance app/Finance app/Profile.swift
+++ Finance app/Finance app/Profile.swift
@@ -9,6 +9,43 @@
 import UIKit
 
 class Profile: NSObject {
+    struct Item {
+        var deposit:Int!
+        var withdrawl:Int!
+    }
+    
+    override init() {
+        super.init()
+    }
+    
+    public static var name:String=""
+    public static var ledger:[Item]=[]
+    public static var income:Int=0
+    public static var expense:Int=0
+    
+    
+    
+    static let getCash=ledger.reduce(0, {x,y in x+y.deposit-y.withdrawl})+income-expense
+    static let getTotalWithdrawls=ledger.reduce(0, {x,y in x+y.withdrawl})
+    static let getTotalIncome=ledger.reduce(0,{x,y in x+y.deposit})
+    
+
+}
+
+class lastProfile: NSObject {
+    struct Item {
+        var deposit:Int!
+        var withdrawl:Int!
+    }
+    
+    override init() {
+        super.init()
+    }
+    
+    public static var name:String=""
+    public static var ledger:[Item]=[]
+    public static var income:Int=0
+    public static var expense:Int=0
     
 
 }
